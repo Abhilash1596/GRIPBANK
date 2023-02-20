121119c81ed9be912be4b9c5c8b1c4876af8febb
Abhilash Bebarta <80850394+Abhilash1596@users.noreply.github.com>
2023-02-20 07:54:10 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GRIP Banking System</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="nabar">
+    </div>
+    <div class="header">
+        <div class="logo">
+                <p style="color: tomato;">G</p>RIP BANK
+        </div>
+        <div class="menu">
+                <ul>
+                    <li style="background-color: rgb(85,166, 48);"><a class="lin" href="index.php">Home</a></li>
+                    <li><a class="lin" href="transaction.php">Transaction History</a></li>
+                    <li><a class="lin" href="user.php">Customers</a></li>
+                    <li><a class="lin" href="https://www.thesparksfoundationsingapore.org">About</a></li>
+                </ul>
+        </div>
+    </div>
+
+    
+    <div class="cons">
+        <p class="salutation">Hello,</p>
+        <p class="p1">I am <strong>Abhilash Bebarta</strong></p>
+        <p class="p1">Web Developer Intern at the Sparks Foundation, batch February 2023.</p>
+        <p class="p1">This is Task 1: Basic Banking System Done in HTML, CSS, and Javascript.</p>
+    </div>
+
+
+    <div class="footer">
+        <p>©2023 Abhilash Bebarta</p>
+    </div>
+</body>
+</html>
